29060e514f721e2a45a7988f799489d2b17aadc6
yuusha10 <amyk12847529@gmail.com>
2024-01-21 13:48:59 +0900

    add post function test

---
+++ app/src/test/java/dojo/socialnetwork/UserTest.java
@@ -0,0 +1,22 @@
+package dojo.socialnetwork;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class UserTest {
+
+    // Posting: Thomas can publish messages a message.
+    @Test
+    void postFunctionTest() {
+        User user = new User("THOMAS");
+        user.post("post publish message");
+
+        List<String> posts = user.fetchAllPost();
+
+        List<String> expected = List.of("post publish message");
+        assertEquals(expected, posts);
+    }
+}
